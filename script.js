45e030310ce62959d7b160ed1d3354decf6a1e32
ediats <ediats@yahoo.com>
2025-05-10 18:52:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,23 +2,38 @@ $(document).ready(function () {
   // Initialize the map centered on Union Station
   var map = L.map('map').setView([39.7527, -104.9992], 13);
 
-  // Add OpenStreetMap baselayer
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  // Add base tile layer
+  var defaultBase = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
   }).addTo(map);
 
-  // Load GeoJSON of Light Rail Stations
+  // Create an empty layer group variable for rail stations
+  var railStations;
+
+  // Load the GeoJSON and add it to the map
   $.getJSON('light_rail_stations.geojson', function (data) {
-    L.geoJSON(data, {
+    railStations = L.geoJSON(data, {
       onEachFeature: function (feature, layer) {
         if (feature.properties && feature.properties.STATION) {
           layer.bindPopup('<strong>' + feature.properties.STATION + '</strong>');
         }
       }
-    }).addTo(map);
+    });
+
+    // Add it to the map
+    railStations.addTo(map);
+
+    // Add the layer to the overlay controls
+    var baseMaps = {
+      "OpenStreetMap": defaultBase
+    };
+    var overlayMaps = {
+      "Light Rail Stations": railStations
+    };
+    L.control.layers(baseMaps, overlayMaps).addTo(map);
   });
 
-  // jQuery interaction: fade out title on click
+  // jQuery interaction - fade out title
   $('#map-title').click(function () {
     $(this).fadeOut('slow');
   });
